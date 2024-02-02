3d29a31e7f8a7906faf33de1b077ce4e629a9ddc
Take <140660037+jiyutake@users.noreply.github.com>
2024-02-02 23:53:29 +0000

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -1,1 +1,9 @@
-{ config, pkgs, ... }:
+{ config, pkgs, ... }: {
+  imports = [
+    ./hardware-configuration.nix
+  ];
+
+  boot.loader.systemd-boot.enable = true; # (for UEFI systems only)
+  fileSystems."/".device = "/dev/disk/by-label/nixos";
+  services.sshd.enable = true;
+}
