d8c5a9d139efa289d8ca414108b735e275e27fc8
Take <140660037+jiyutake@users.noreply.github.com>
2024-02-02 03:43:36 +0000

    Delete home directory

--- home/config/nvim/init.lua
+++
@@ -1,8 +0,0 @@
--- Include packer's plugin configuration and remapped bindings
-
-require("toki.packer")
-require("toki.remap")
-
--- The colorscheme to be used.
--- Available: 'oxocarbon', 'catpuccin-latte', 'onedark'
-vim.cmd('colorscheme onedark')
