e82f475644515c38c0d97117e2da5730dff4df1f
Take <140660037+jiyutake@users.noreply.github.com>
2024-02-03 00:22:05 +0000

    Create theme.lua

---
+++ config/awesome/theme.lua
@@ -0,0 +1,10 @@
+-- Lib
+local gears = require('gears')
+local dpi = require('beautiful').xresources.apply_dpi
+local gfs = require('gears.filesystem')
+
+-- Path to open assets
+local path = gfs.get_configuration_dir() .. 'assets/'
+
+-- Define colors used as a theme
+local _T = {}
