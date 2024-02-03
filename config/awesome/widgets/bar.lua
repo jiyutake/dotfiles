cfd38e244853e392aad6628539034353da86fe9f
Take <140660037+jiyutake@users.noreply.github.com>
2024-02-03 00:52:08 +0000

    Create bar.lua

---
+++ config/awesome/widgets/bar.lua
@@ -0,0 +1,12 @@
+-- Libs
+local wibox = require('wibox')
+local awful = require('awful')
+local naughty = require("naughty")
+local beautiful = require('beautiful')
+local dpi = beautiful.xresources.apply_dpi
+local bling = require('lib.bling')
+local launcher = require('widgets.launcher')
+local gettags = require('widgets.taglist')
+local user = require('user')
+
+--code or whatever seoigvjkrgvb
