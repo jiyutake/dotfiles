155c7b6824443feca7de34948bfc12d78f24b472
Take <140660037+jiyutake@users.noreply.github.com>
2024-02-03 00:03:27 +0000

    Delete configuration.nix

--- configuration.nix
+++
@@ -1,8 +0,0 @@
-{ config, pkgs, ... }: {
-  imports = [
-    ./hardware-configuration.nix
-  ];
-
-  boot.loader.systemd-boot.enable = true; # (for UEFI systems only)
-  fileSystems."/".device = "/dev/disk/by-label/nixos";
-}
