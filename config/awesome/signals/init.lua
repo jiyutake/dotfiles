e6a7faef37aafb409490f29dc0f775d11f794eac
Take <140660037+jiyutake@users.noreply.github.com>
2024-02-03 00:53:10 +0000

    Create init.lua

---
+++ config/awesome/signals/init.lua
@@ -0,0 +1,9 @@
+-- This is a init.lua file.
+-- Make sure if you add any file in this folder that you include it here.
+require('signals.network')
+require('signals.client')
+require('signals.rules')
+require('signals.layouts')
+require('signals.wallpaper')
+require('signals.gunshot')
+require('signals.caps')
