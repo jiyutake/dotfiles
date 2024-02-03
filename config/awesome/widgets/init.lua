39302515cca0a3d0374998e2afb060043d2951ce
Take <140660037+jiyutake@users.noreply.github.com>
2024-02-03 00:51:41 +0000

    Create init.lua

---
+++ config/awesome/widgets/init.lua
@@ -0,0 +1,6 @@
+-- This is a init.lua file
+-- Make sure if you add any file in this folder that you include it here.
+require('widgets.bar')
+require('widgets.titlebar')
+require('widgets.notif')
+require('widgets.music')
