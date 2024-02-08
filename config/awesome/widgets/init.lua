07e60415c7eea7acc183935d86c639a1ef0c2956
Take <140660037+jiyutake@users.noreply.github.com>
2024-02-08 22:34:03 +0000

    Delete config/awesome/widgets/init.lua

--- config/awesome/widgets/init.lua
+++
@@ -1,6 +0,0 @@
--- This is a init.lua file
--- Make sure if you add any file in this folder that you include it here.
-require('widgets.bar')
-require('widgets.titlebar')
-require('widgets.notif')
-require('widgets.music')
