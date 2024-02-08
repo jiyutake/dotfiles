aa22a20e626a34652263435d77f4d5e86f3bcacc
Take <140660037+jiyutake@users.noreply.github.com>
2024-02-08 22:34:44 +0000

    Delete config/awesome/signals/init.lua

--- config/awesome/signals/init.lua
+++
@@ -1,9 +0,0 @@
--- This is a init.lua file.
--- Make sure if you add any file in this folder that you include it here.
-require('signals.network')
-require('signals.client')
-require('signals.rules')
-require('signals.layouts')
-require('signals.wallpaper')
-require('signals.gunshot')
-require('signals.caps')
