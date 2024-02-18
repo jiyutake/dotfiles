da22017801bdf72bd0ebf6b7511062442a33d874
Take <140660037+jiyutake@users.noreply.github.com>
2024-02-18 19:21:20 +0000

    hopefully it works(won't test yet!)

--- home-manager/programs/bspwm/picom/default.nix
+++ home-manager/programs/bspwm/picom/default.nix
@@ -1,7 +1,32 @@
-{ config, ... }:
+{ config, inputs, ... }:
 
 {
 
-  # soon(tm)
+  services.picom = {
+
+    enable=false;
+    activeOpacity = 1,0;
+    backend = "glx";
+    fade = ;
+    fadeDelta =;
+    fadeSteps = [ 0.03 0.03 ];
+    opacityRules = [
+
+      #
+
+    ];
+
+    settings = {
+
+      rounded-corners-exclude = [];
+      blur-background-exclude = [];
+      blur-background = false;
+      blur-background-frame = false;
+      blur-background-fixed = false;
+      glx-no-stencil = true;
+
+    };
+
+  };
 
 }
