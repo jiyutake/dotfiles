fdbf9831af090c749c4f4a005c3995b194db62f5
Take <140660037+jiyutake@users.noreply.github.com>
2024-02-18 02:32:56 +0000

    going insane.nix

--- home-manager/flake.nix
+++ home-manager/flake.nix
@@ -1,29 +1,7 @@
 {
-  description = "Home Manager configuration of tiffany";
 
-  inputs = {
-    # Specify the source of Home Manager and Nixpkgs.
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-  };
+  inputs = { "woof" };
 
-  outputs = { nixpkgs, home-manager, ... }:
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in {
-      homeConfigurations."tiffany" = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
+  outputs = { "meow" };
 
-        # Specify your home configuration modules here, for example,
-        # the path to your home.nix.
-        modules = [ ./home.nix ];
-
-        # Optionally use extraSpecialArgs
-        # to pass through arguments to home.nix
-      };
-    };
 }
